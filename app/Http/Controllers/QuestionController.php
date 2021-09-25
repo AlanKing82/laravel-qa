77a1dfdf1c6dc86231655e05d61d654f6a3289ae
Alan King <kingpalan@yahoo.co.uk>
2021-09-25 08:05:19 +0100

    Add votes, compile assets

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class QuestionController extends Controller
 {
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,17 @@
                 <div class="card-body">
                     @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes ) }}
+                                </div>
+                                <div class="status">
+                                    <strong>{{ $question->answers }}</strong> {{ Str::plural('answer', $question->answers ) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views . " " . Str::plural('view', $question->views ) }}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <!-- <h3 class="mt-0"><a href="{{ route('questions.show', $question->id )}}">{{ $question->title }}</a></h3> -->
@@ -18,7 +29,7 @@
                                     <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                     <small class="text-muted">{{ $question->created_date }}</small>
                                 </p>
-                                {{ \Illuminate\Support\Str::limit($question->body, 250) }}
+                                {{ Str::limit($question->body, 250) }}
                             </div>
                         </div>
                         <hr>
