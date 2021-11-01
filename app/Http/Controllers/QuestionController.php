bf4cc403f4d1a4d98fa7e32cd2d866aa9e1cf537
Alan King <kingpalan@yahoo.co.uk>
2021-11-01 09:42:54 +0000

    Delete question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -96,8 +96,10 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+         return redirect('/questions')->with('success', 'Your question has been deleted');
     }
 }
